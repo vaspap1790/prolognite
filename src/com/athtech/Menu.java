bceb1e04b2e1201855b2dab0ae2bfcba9332239c
vaspap1790 <vaspap1790@gmail.com>
2020-04-06 09:19:37 +0300

    navigation2

--- src/com/athtech/Menu.java
+++ src/com/athtech/Menu.java
@@ -21,6 +21,8 @@ public class Menu {
             input = Main.scanner.nextLine();
 
             switch(input) {
+                case "0":
+                    break;
                 case "1":
                     question1();
                     break;
@@ -35,10 +37,11 @@ public class Menu {
         System.out.println("\n******************************* You ended the application *********************************");
     }
 
-    public static void question1(){
+    public static void question1() throws ParseException {
 
         String input;
 
+        do {
         System.out.println("\n");
         System.out.println("*******************************************************************************************");
         System.out.println("**                                   -- QUESTION 1 --                                    **");
@@ -46,18 +49,31 @@ public class Menu {
         System.out.println("\n");
         System.out.println("-For Question 1 of the Handout, type ..................................................'1'-");
         System.out.println("-For Question 2 of the Handout, type ..................................................'2'-");
-        System.out.println("-If you want to end the application, type .............................................'0'-");
+        System.out.println("-If you want to return to Main Menu, type .............................................'0'-");
         System.out.println("\n");
         System.out.println("*******************************************************************************************");
 
         input = Main.scanner.nextLine();
+            switch(input) {
+                case "0":
+                    break;
+                case "1":
+                    break;
+                case "2":
+                    question2();
+                    break;
+                default:
+                    System.out.println("\nInvalid input. Try again.\n");
+            }
+        } while(!"0".equals(input));
 
     }
 
-    public static void question2(){
+    public static void question2() throws ParseException {
 
         String input;
 
+        do {
         System.out.println("\n");
         System.out.println("*******************************************************************************************");
         System.out.println("**                                 -- QUESTION 2 --                                      **");
@@ -65,10 +81,22 @@ public class Menu {
         System.out.println("\n");
         System.out.println("-For Question 1 of the Handout, type ..................................................'1'-");
         System.out.println("-For Question 2 of the Handout, type ..................................................'2'-");
-        System.out.println("-If you want to end the application, type .............................................'0'-");
+        System.out.println("-If you want to return to Main Menu, type .............................................'0'-");
         System.out.println("\n");
         System.out.println("*******************************************************************************************");
 
         input = Main.scanner.nextLine();
+            switch(input) {
+                case "0":
+                    break;
+                case "1":
+                    break;
+                case "2":
+                    question2();
+                    break;
+                default:
+                    System.out.println("\nInvalid input. Try again.\n");
+            }
+        } while(!"0".equals(input));
     }
 }
